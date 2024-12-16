358aa89d4fcf439cd9c805e6d6744f50817fe689
SumDeusVitae <kyrylosorokovskyi@gmail.com>
2024-12-16 12:11:19 -0600

    P: Revert K
    This reverts commit babf7b73a47cab67ec75c3f29fd55d53045b6ccd.
    
    Revert "K: scan done"
    
    This reverts commit cd62cf2e848857c43caded1186de47bf81499f4a.

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,13 +1,1 @@
-echo -e "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
-grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "========= CREDIT CARD SCAN COMPLETE =========="
-echo -e "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ===="
-grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
-echo -e "\n========= SCANNING FOR PHONE NUMBERS ========="
-grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
-grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
-echo "========= PHONE NUMBER SCAN COMPLETE ========="
-
-# Use the apux language to run the `scan.sh` script
-./scripts/apux.sh ./scripts/scan.sh | grep ' sh'
+# TODO: write the script
